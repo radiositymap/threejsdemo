d7ce61940c4a28726b6d25a3f57a83b9370f02c5
radiositymap <radiositymap@gmail.com>
2024-09-11 22:33:19 +0800

    add wireframe, messages, remap materials

--- src/main.js
+++ src/main.js
@@ -4,12 +4,17 @@ import {MTLLoader} from '../lib/examples/jsm/loaders/MTLLoader.js';
 import {OBJLoader} from '../lib/examples/jsm/loaders/OBJLoader.js';
 import {OrbitControls} from '../lib/examples/jsm/controls/OrbitControls.js';
 
-function loadModel(matPath, objPath, x, y, z, scene, rootName) {
-    console.log('Loading material from ' + matPath);
-    console.log('Loading model from ' + objPath);
-    const loader = new MTLLoader();
-    loader.load(
-        matPath,
+const currDir = window.location.href;
+
+function setLoadingMessage(message, request) {
+    var progress = Math.round(request.loaded/request.total*100);
+    document.querySelector('#message').innerHTML = message + progress + '%';
+    if (progress == 100)
+        document.querySelector('#message').innerHTML = "";
+}
+
+function loadModel(matPath, objPath, x, y, z, scene, rootName) { const loader = new MTLLoader();
+    loader.load(matPath,
         function(materials) {
             materials.preload();
             const objLoader = new OBJLoader();
@@ -19,14 +24,17 @@ function loadModel(matPath, objPath, x, y, z, scene, rootName) {
                 function(object) {
                     object.name = rootName;
                     scene.add(object);
-                }, function(progress) { console.log((progress.loaded/progress.total*100) + '%'); },
+                },
+                function(request) {
+                    setLoadingMessage('Loading materials... ', request);
+                },
                 function(error) {
                     console.log('Error while loading OBJ: ' + error);
                 }
             );
         },
-        function(progress) {
-            console.log((progress.loaded/progress.total*100)+'%');
+        function(request) {
+            setLoadingMessage('Loading model... ', request);
         },
         function(error) {
             console.log('Error while loading MTL: ' + error);
@@ -36,14 +44,11 @@ function loadModel(matPath, objPath, x, y, z, scene, rootName) {
 
 class Resizer {
     setSize = (container, camera, renderer) => {
-        const h = container.clientHeight;
-        const w = container.clientWidth;
-        camera.aspect = w/h;
+        camera.aspect = container.clientWidth/container.clientHeight;
         camera.updateProjectionMatrix();
-        renderer.setSize(w, h);
+        renderer.setSize(container.clientWidth, container.clientHeight);
         renderer.setPixelRatio(window.devicePixelRatio);
     };
-
     constructor(container, camera, renderer) {
         this.setSize(container, camera, renderer);
         window.addEventListener('resize', () => {
@@ -84,9 +89,9 @@ class Loop {
 
 function createWorld(container) {
     const camera = new THREE.PerspectiveCamera(60, 1, 0.1, 100);
-    camera.position.set(0, 0, 10);
+    camera.position.set(0, 0, 5);
 
-    const renderer = new THREE.WebGLRenderer({ antialias: true});
+    const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true});
     renderer.physicallyCorrectLights = true;
 
     const scene = new THREE.Scene();
@@ -103,7 +108,7 @@ function createWorld(container) {
     const dirLight = new THREE.DirectionalLight('white', 3);
     const dirLight2 = new THREE.DirectionalLight('white', 5);
     dirLight.position.set(0, 0, -1);
-    dirLight2.position.set(1, 0, 1);
+    dirLight2.position.set(1, 0, 1.2);
 
     loop.updatables.push(controls);
     scene.add(ambientLight, dirLight, dirLight2);
@@ -112,20 +117,53 @@ function createWorld(container) {
     return {camera, renderer, scene, loop};
 }
 
-function render(renderer, scene, camera) {
-    renderer.render(scene, camera);
-}
+function createUI(model, materials) {
+    const panel = new GUI({width:250});
 
-function createUI(model) {
-    const panel = new GUI({width:280});
-    const folder = panel.addFolder('Model');
+    const settingsFolder = panel.addFolder('Settings');
+    const settings = {
+        renderMode: 0,
+        wireframeColour: 0x93ffe8
+    }
+    const meshes = model.children;
+    settingsFolder.add(settings, 'renderMode', {Lit: 0, Wireframe: 1})
+        .name('Render Mode')
+        .onChange(val => {
+            if (val == 0) {
+                for (var i=0; i<meshes.length; i++)
+                    meshes[i].material = materials[meshes[i].name];
+            }
+            if (val == 1) {
+                const wireMat = new THREE.MeshBasicMaterial({
+                    color: settings.wireframeColour,
+                    wireframe: true,
+                    wireframeLinewidth: 0.2
+                });
+                for (var i=0; i<meshes.length; i++)
+                    meshes[i].material = wireMat;
+            }
+         });
+    settingsFolder.addColor(settings, 'wireframeColour')
+        .name('Wireframe Colour')
+        .onChange(val => {
+            if (settings.renderMode == 1) {
+                const wireMat = new THREE.MeshBasicMaterial({
+                    color: val,
+                    wireframe: true,
+                    wireframeLinewidth: 0.2
+                });
+                for (var i=0; i<meshes.length; i++)
+                    meshes[i].material = wireMat;
+            }
+        });
+
+    const modelFolder = panel.addFolder('Model');
     var components = {};
-    for (var i=0; i<model.children.length; i++) {
-        var child = model.children[i];
-        components[child.name] = child.material.name;
-        if (child.material.name === undefined)
-            components[child.name] = 'null';
-        folder.add(components, child.name);
+    for (var i=0; i<meshes.length; i++) {
+        components[meshes[i].name] = meshes[i].material.name;
+        if (meshes[i].material.name === undefined)
+            components[meshes[i].name] = 'null';
+        modelFolder.add(components, meshes[i].name);
     }
 }
 
@@ -133,64 +171,73 @@ function delay(time) {
     return new Promise(resolve => setTimeout(resolve, time));
 }
 
-async function main() {
-    const container = document.querySelector('#scene-container');
-    const {camera, renderer, scene, loop} = createWorld(container);
-    const currDir = window.location.href;
+function remapMaterials(model) {
 
-    const modelPath = currDir + '/../assets/PRSModel/PRSModel.obj';
-    const materialPath = currDir + '/../assets/PRSModel/PRSModel.mtl';
-    loadModel(materialPath, modelPath, 0, 0, 0, scene, 'scene_root');
-    var model = null;
-    do {
-        model = scene.getObjectByName('scene_root');
-        await delay(1000);
-    } while (model == null);
-
-    createUI(model);
-
-    // remap materials
+    // top
+    const topTexPath = currDir + '/../assets/PRSModel/PRSTop.jpg';
+    const textureTop = new THREE.TextureLoader().load(topTexPath);
+    textureTop.colorSpace = THREE.SRGBColorSpace;
+    const topMat = new THREE.MeshPhysicalMaterial({
+            color: 0xffffff,
+            metalness: 0,
+            roughness: 0.1,
+            map: textureTop,
+            clearcoat: 0.35,
+            clearcoatRoughness: 0,
+            reflectivity: 0.8
+        });
+    topMat.name = 'Top';
+    model.getObjectByName('Plane').material[1] = topMat;
 
     // neck
     const woodTexPath = currDir + '/../assets/PRSModel/NeckWood.jpg';
-    const texture = new THREE.TextureLoader().load(woodTexPath);
-    texture.wrapS = THREE.RepeatWrapping;
-    texture.wrapT = THREE.RepeatWrapping;
-    texture.repeat.set(5, 1);
-    model.getObjectByName('Cube.004_Cube.005').material =
-        new THREE.MeshPhysicalMaterial({
+    const textureWood = new THREE.TextureLoader().load(woodTexPath);
+    textureWood.wrapS = THREE.RepeatWrapping;
+    textureWood.wrapT = THREE.RepeatWrapping;
+    textureWood.repeat.set(5, 1);
+    const woodMat = new THREE.MeshPhysicalMaterial({
             color: 0xdddddd,
             metalness: 0.0,
             roughness: 0.8,
-            map: texture
+            map: textureWood
         });
+    woodMat.name = 'Wood';
+    model.getObjectByName('Cube.004_Cube.005').material = woodMat;
 
     // logo
-    const logoTexPath = currDir + '/../assets/PRSModel/PRSLogo.jpg';
+    const logoTexPath = currDir + '/../assets/PRSModel/PRSLogo.png';
     const textureLogo = new THREE.TextureLoader().load(logoTexPath);
-    textureLogo.center.set(0.5, 0.5);
-    textureLogo.rotation = -Math.PI/2;
-    model.getObjectByName('Plane.005_Plane.007').material =
-        new THREE.MeshPhysicalMaterial({
+    textureLogo.colorSpace = THREE.SRGBColorSpace;
+    const logoMat = new THREE.MeshPhysicalMaterial({
             color: 0x000000,
-            alphaMap: textureLogo,
+            map: textureLogo,
             transparent: true,
             opacity: 1
         });
+    logoMat.name = 'Logo';
+    model.getObjectByName('Plane.005_Plane.007').material = logoMat;
 
     // birds
     const birdsTexPath = currDir + '/../assets/PRSModel/PRSBirds.png';
     const textureBirds = new THREE.TextureLoader().load(birdsTexPath);
+    const birdsNormPath = currDir + '/../assets/PRSModel/FoilNormal.jpg';
+    const normBirds = new THREE.TextureLoader().load(birdsNormPath);
+    normBirds.wrapS = THREE.RepeatWrapping;
+    normBirds.wrapT = THREE.RepeatWrapping;
+    normBirds.repeat.set(5, 1);
     const birdMat = new THREE.MeshPhysicalMaterial({
-            color: 0xaaddff,
+            color: 0xbdedff,
             metalness: 1.0,
             roughness: 0.5,
+            normalMap: normBirds,
+            normalMapType: THREE.ObjectSpaceNormalMap,
             alphaMap: textureBirds,
             transparent: true,
             opacity: 1.0,
             iridescence: 1.0,
-            iridescenceIOR: 1.5
+            iridescenceIOR: 1.2
         });
+    birdMat.name = 'Birds';
     model.getObjectByName('Plane.007_Plane.019').material = birdMat;
     model.getObjectByName('Plane.009_Plane.010').material = birdMat;
     model.getObjectByName('Plane.010_Plane.011').material = birdMat;
@@ -202,14 +249,66 @@ async function main() {
     model.getObjectByName('Plane.016_Plane.017').material = birdMat;
     model.getObjectByName('Plane.017_Plane.018').material = birdMat;
 
-    //model.getObjectByName('glass').material =
-    //    new THREE.MeshPhysicalMaterial({
-    //        color: 0xAAAAAA,
-    //        metalness: 0.25,
-    //        roughness: 0,
-    //        transmission: 1.0
-    //    });
-    //scene.add(model);
+    // metal
+    const metalMat = new THREE.MeshPhysicalMaterial({
+            color: 0xbbbbbb,
+            metalness: 1.0,
+            roughness: 0.15,
+            reflectivity: 0.9,
+            opacity: 1
+        });
+    metalMat.name = 'Metallic';
+    for (var i=0; i<model.children.length; i++) {
+        if (model.children[i].material.name != undefined &&
+                model.children[i].material.name == 'Metal') {
+            model.children[i].material = metalMat;
+        }
+    }
+    model.getObjectByName('Cylinder.024_Cylinder.025').material[3] = metalMat;
+    model.getObjectByName('Cylinder.025_Cylinder.027').material[3] = metalMat;
+
+    // glass
+    const glassMat = new THREE.MeshPhysicalMaterial({
+            color: 0xffffff,
+            metalness: 0,
+            roughness: 0.1,
+            transparent: true,
+            transmission: 0.7,
+            opacity: 0.3,
+            ior: 1.4
+        });
+    glassMat.name = 'Glassy';
+    model.getObjectByName('Cylinder.024_Cylinder.025').material[1] = glassMat;
+    model.getObjectByName('Cylinder.025_Cylinder.027').material[1] = glassMat;
+}
+
+function saveMaterials(model) {
+    var materials = {};
+    for (var i=0; i<model.children.length; i++)
+        materials[model.children[i].name] = model.children[i].material;
+    return materials;
+}
+
+async function main() {
+    const container = document.querySelector('#scene-container');
+    const {camera, renderer, scene, loop} = createWorld(container);
+
+    const modelPath = currDir + '/../assets/PRSModel/PRSModel.obj';
+    const materialPath = currDir + '/../assets/PRSModel/PRSModel.mtl';
+    loadModel(materialPath, modelPath, 0, 0, 0, scene, 'scene_root');
+    var model = null;
+    do {
+        model = scene.getObjectByName('scene_root');
+        await delay(1000);
+    } while (model == null);
+
+    document.querySelector('#footer').innerHTML =
+        'Model and textures from ' +
+        'https://www.cgtrader.com/designers/bootbadger04';
+
+    remapMaterials(model);
+    const materials = saveMaterials(model);
+    createUI(model, materials);
 
     loop.start();
 }
